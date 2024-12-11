407173573d858d2f9076361d37771bcf71820e7e
Thomaz <thomaz2pinheiro@gmail.com>
2024-12-11 12:04:12 -0300

    shoot when pressing continuously

--- scenes/enemies/asteroid.gd
+++ scenes/enemies/asteroid.gd
@@ -1,11 +1,12 @@
 extends CombatCharacter3D
 
-
-
-
-
-
+@export var min_size: float = 0.5
+@export var max_size: float = 2.0
 
 func _physics_process(delta: float) -> void:
 	velocity = global_basis.z * 3.0
 	move_and_slide()
+
+func change_to_random_size() -> void:
+	var random_size = randf_range(min_size, max_size)
+	scale = Vector3(random_size, random_size, random_size)
--- scenes/map/spawner.gd
+++ scenes/map/spawner.gd
@@ -4,11 +4,11 @@ extends Node3D
 @onready var current_level=0
 #the level is the key the amount of monsters is the value
 @onready var monster_dict={
- 1:1, # level:amount_of_monsters
- 2:2,
- 3:4,
- 4:8,
- 5:30
+ 1:160, # level:amount_of_monsters
+ 2:320,
+ 3:640,
+ 4:1000,
+ 5:2000
 }
 
 #the monster we will be spawning in. 
@@ -35,6 +35,7 @@ func enemy_death():
 func spawn_enemies():
  for i in range(monster_dict[current_level]):
   var m : CombatCharacter3D = monster.instantiate()
+  m.change_to_random_size()
   print("spawning enemy")
   #we check the amount of children on our spawn holder 
   var spawn_length = self.get_child_count()-1
@@ -48,14 +49,13 @@ func spawn_enemies():
   m.killed.connect(enemy_death)
   add_child(m)
   #This is just a fast way to create a timer similar to wait in python
-  await get_tree().create_timer(0.5).timeout
+  await get_tree().create_timer(0.1).timeout
   
 
 func update_level(level):
  match level:
   1:
    print("its level one")
-   #additional stuff here like announcer saying: Wave 1!
   2:
    print("its level two")
   3:
@@ -70,5 +70,6 @@ func update_level(level):
 
 func _on_in_between_waves_timeout():
  print("Leaving level: ", current_level)
- current_level+=1
- update_level(current_level)
+ if current_level <= 4:
+   current_level+=1
+   update_level(current_level)
