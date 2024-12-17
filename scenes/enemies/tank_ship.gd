4f69e62318dd69afac5fde6db68a606403690301
Natan Maia <natanmaiaplays@outlook.com>
2024-12-17 20:03:53 -0300

    new tank ship

--- scenes/enemies/tank_ship.gd
+++ scenes/enemies/tank_ship.gd
@@ -4,6 +4,7 @@ extends CombatCharacter3D
 
 @export var packed_bullet : PackedScene = preload("res://scenes/enemies/enemy_ship_bullet.tscn")
 
+var shoot_angle_degrees : float = 0.0
 
 
 func _ready() -> void:
@@ -16,3 +17,6 @@ func shoot():
 	get_tree().current_scene.add_child(bullet)
 	bullet.global_position = self.global_position
 	bullet.global_rotation = self.global_rotation
+	bullet.rotate_x(deg_to_rad(shoot_angle_degrees))
+	
+	shoot_angle_degrees += 30
