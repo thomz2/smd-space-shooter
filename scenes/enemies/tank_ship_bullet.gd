659bf4ec627986d94ce2d2470a1e101f6ee8915a
Thomaz <thomaz2pinheiro@gmail.com>
2025-01-07 23:22:27 -0300

    big bullet changing collor on reflection

--- scenes/enemies/tank_ship_bullet.gd
+++ scenes/enemies/tank_ship_bullet.gd
@@ -1,6 +1,9 @@
 extends Bullet3D
 
-
-
 func on_reflected() -> void:
 	main_hitbox.damage = 50
+	
+	for child in $ModelOrigin.get_children():
+		if child is MeshInstance3D:
+			child.mesh.material = child.mesh.material.duplicate(true)
+			child.mesh.material.albedo_color = Color(0, 1, 1)
