a6b875a587df0fa379177e6bf7802d5564ac735f
Natan Maia <natanmaiaplays@outlook.com>
2025-01-29 11:01:44 -0300

    fix wave bug

--- scenes/screens/wave_selection.gd
+++ scenes/screens/wave_selection.gd
@@ -4,9 +4,10 @@ extends Panel
 @onready var button_decrease: Button = $LeftButton
 
 func _ready() -> void:
-	GameManager.wave = 0
+	GameManager.wave = 1
 	button_increase.pressed.connect(_on_button_increase_pressed)
 	button_decrease.pressed.connect(_on_button_decrease_pressed)
+	update_state()
 	
 func _on_button_increase_pressed() -> void:
 	GameManager.wave += 1
